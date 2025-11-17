74d481c23f428ce77d75a02c49d595c4f5b60c80
Sarvajith Adyanthaya <109269103+Chillthrower@users.noreply.github.com>
2025-11-16 19:56:12 -0800

    Update Server.py

--- Lab/Lab8/Server.py
+++ Lab/Lab8/Server.py
@@ -5,24 +5,27 @@ arp_table = {}
 s = socket.socket()
 s.bind(("127.0.0.1", 9001))
 s.listen()
+s.settimeout(1)
 print("[S] listening on 127.0.0.1:9001")
 time.sleep(2)
 print("[S] client connected: ('127.0.0.1', 9001)")
 
 while True:
-    c, _ = s.accept()
-    msg = c.recv(4096).decode().strip()
-    print(f"[S] recv: {msg}")
+    try:
+        c, _ = s.accept()
+        msg = c.recv(4096).decode().strip()
+        print(f"[S] recv: {msg}")
 
-    if msg == "quit":
-        print("[S] Server Stopped")
-        break
+        if msg == "quit":
+            print("[S] Server Stopped")
+            break
 
-    if msg.startswith("ARP:"):
-        _, ip, mac = msg.split(":", 2)
-        if ip in arp_table and arp_table[ip] != mac:
-            print(f"!!! ALERT: IP {ip} seen with MACs {mac}, {arp_table[ip]}")
-        arp_table[ip] = mac
+        if msg.startswith("ARP:"):
+            _, ip, mac = msg.split(":", 2)
+            if ip in arp_table and arp_table[ip] != mac:
+                print(f"!!! ALERT: IP {ip} seen with MACs {mac}, {arp_table[ip]}")
+            arp_table[ip] = mac
 
-    c.sendall(b"ACK")
-    c.close()
+        c.sendall(b"ACK")
+        c.close()
+    except TimeoutError: pass
